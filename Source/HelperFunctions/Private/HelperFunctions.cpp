1def4dd51419c0f5095c9b8064a26b21f056d4ce
marcusonline97 <Mjpersson97@gmail.com>
2026-07-05 01:48:45 +0200

    Added some basic modules we can expand on later

---
+++ Source/HelperFunctions/Private/HelperFunctions.cpp
@@ -0,0 +1,17 @@
+﻿#include "HelperFunctions.h"
+
+#define LOCTEXT_NAMESPACE "FHelperFunctionsModule"
+
+void FHelperFunctionsModule::StartupModule()
+{
+    
+}
+
+void FHelperFunctionsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FHelperFunctionsModule, HelperFunctions)
---
+++ Source/Multiplayer/Private/Multiplayer.cpp
@@ -0,0 +1,17 @@
+﻿#include "Multiplayer.h"
+
+#define LOCTEXT_NAMESPACE "FMultiplayerModule"
+
+void FMultiplayerModule::StartupModule()
+{
+    
+}
+
+void FMultiplayerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FMultiplayerModule, Multiplayer)
