dd9dad3fc79f0f9e6e85779b47be1802edfdb2b1
marcusonline97 <Mjpersson97@gmail.com>
2026-07-11 23:49:37 +0200

    Secret Update

---
+++ Source/BackroomsCamera/Private/BackroomsCamera.cpp
@@ -0,0 +1,17 @@
+﻿#include "BackroomsCamera.h"
+
+#define LOCTEXT_NAMESPACE "FBackroomsCameraModule"
+
+void FBackroomsCameraModule::StartupModule()
+{
+    
+}
+
+void FBackroomsCameraModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FBackroomsCameraModule, BackroomsCamera)
